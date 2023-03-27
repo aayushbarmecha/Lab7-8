77f81504e0630a33f4ef2ea2c09086a978b0b7d0
Aayush Barmecha <barmecha99@gmail.com>
2023-03-27 15:17:50 +0530

    Added all the files

---
+++ compilers.js
@@ -0,0 +1,51 @@
+//jshint esversion:6
+const mysql = require('mysql');
+const express = require("express");
+const ejs = require('ejs');
+const bodyParser = require("body-parser");
+const { execFile } = require('node:child_process');
+var fs = require('fs');
+
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:"true"}));
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"MyNewPass",
+    database:"aayush"
+});
+
+app.get("/" , function(req,res){
+    res.sendFile(__dirname + "/home.html");
+});
+
+app.post("/",function(req,res){
+    var content = req.body.command1;
+    fs.writeFile('input.txt', content, err => {
+        if (err) {
+            console.error(err);
+        } 
+    })
+   
+    const child = execFile('./q1', (error, stdout, stderr) => {
+        if (error) {
+          throw error;
+        }
+        var data = fs.readFileSync('out.txt', 'utf8');
+        
+
+        connection.query(data, function (err, results, fields) {
+            if (err) throw err;
+            res.render("output", {results: results});
+        });
+    })
+});
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000");
+});
